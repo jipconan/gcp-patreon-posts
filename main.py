091c868afd637d5e087ae61895a5d74460c7a2b7
jipconan <jipconan@gmail.com>
2025-01-05 21:29:38 +0800

    adjustment/adjustment_v002

--- main.py
+++ main.py
@@ -53,5 +53,5 @@ def main():
         return jsonify({"error": str(e)}), 500
 
 # Run Flask locally
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
